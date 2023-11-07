aeab98cdc7be16b4a93490beac47dd7fa6773423
Mahadi Hasan <42115266+mh16169@users.noreply.github.com>
2023-11-07 12:06:55 +0600

    Add files via upload

---
+++ 1059 Even Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+/*int a;
+scanf("%d", &a)
+*/
+
+for(int i = 0; i<= 100; i += 2 ){
+
+    //if(i% 2 == 0) /other solution formula
+    printf("%d\n", i);
+
+}
+
+return 0;
+}
